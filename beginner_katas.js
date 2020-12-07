1b0a906f0d4a4e6d4732b7efa9e126f14d6ecff5
Grizzly <b.mullerjnr@gmail.com>
2020-12-07 23:18:04 +0200

    'bus'

--- beginner_katas.js
+++ beginner_katas.js
@@ -31,26 +31,80 @@ function toCamelCase(str){
 
 
 //"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
-function order(words){
-    let checked_num = 0;
+//in progress
+function order2(words){    
     let strArr = words.split(' ');
-    console.log(strArr)
-    let temp = '';
-    for (let i = 0; i < strArr.length +1; i++) {
-        if( strArr.indexOf(String(checked_num)) >= 0) {
-            temp = temp + strArr[i] + ' ';
+    console.log(strArr);
+    let temp = [];
+    strArr.forEach(element => {
+        // console.log('check1');
+        let found = false;
+        let checked_num = 0;
+        for (let j = 0; j < element.length +1; j++) {
+        //   console.log('check2');
+          if( element.indexOf(String(checked_num)) >= 0) found = true;
+            checked_num += 1;
+        }
+        if (found) {
+            temp.push(strArr[element]);  
+            console.log(element)          
         } else {
-            console.log('what')
+            console.log('what');
+        }            
+    });  
+    return temp.join('');
+}
+
+function order(words) {
+    let strArr = words.split(' '); // ['is2', 'Thi1s', 'T4est', '3a']
+    let allFound = false;
+    let checked_num = 0;
+    let temp = [];
+    while (allFound === false) {
+        let amountFound = 0;
+        let found = false;
+        strArr.forEach(element =>{
+            if( element.indexOf(checked_num) >= 0) found = true;                        
+            if (found) {
+                console.log(checked_num.typeOf);
+                console.log('found');
+                temp.push(element);
+                amountFound++;
+            } else {
+                console.log('nope');
+            }
+        });        
+        if (amountFound === strArr.length) {
+            allFound = true;
+        } else {
+            console.log('still going');
         }
         checked_num += 1;
     }
-    return temp
+    console.log('done');
+    console.log(temp.join(' '));
 }
 
 
-
-let reverse = reverseString('Hello world');
-let reverse2 = reverseAgain('Did it again');
-let camelCase = toCamelCase('this is camel');
-
-console.log(order("is2 Thi1s T4est 3a"))
+// Function that checks whether Bob the bus driver has enough space on his bus for waiting passengers
+//Learned MAth.abs
+//Learned `` strings
+function enough(cap, on, wait) {
+    let space = cap - on;
+    if (wait > space) {
+        console.log(`${Math.abs(wait-cap)}, He can't fit ${Math.abs(space-wait)} out of ${wait} waiting.`);
+    } else if (wait === space){
+        console.log(`0, He can fit all ${wait} passengers`);
+    } else {
+        console.log(`He can fit all ${wait} waiting passengers with ${space-wait} space left`);
+    }
+}
+// codewars solution
+function enough(cap, on, wait) {
+    let space = cap - on;
+    if (wait > space) {
+        return Math.abs(space-wait);
+    } else {
+        return 0;
+    }
+}
